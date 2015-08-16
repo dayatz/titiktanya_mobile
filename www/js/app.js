ff717fa61bfdb82aa683308b3162c39f0f2ff2e1
dayatz <dayat.py@gmail.com>
2015-08-16 20:16:39 +0800

    integrate with ionic-material, header, add the side menu

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('TitikTanya', ['ionic', 'TitikTanya.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,33 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider){
+    // Turn off caching for demo simplicity's sake
+    $ionicConfigProvider.views.maxCache(0);
+
+    // Turn off back button text
+    $ionicConfigProvider.backButton.previousTitleText(false);
+
+    $stateProvider
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/side-menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
+            },
+            'fabContent': ''
+        }
+    })
+
+    $urlRouterProvider.otherwise('/app/home');
 })
---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+/**
+* TitikTanya.controllers Module
+*
+* Description
+*/
+angular.module('TitikTanya.controllers', [])
+
+.controller('AppCtrl', ['$scope', function($scope){
+    $scope.isExpanded = false;
+}])
+
+.controller('HomeCtrl', ['$scope', function($scope){
+    
+}])
